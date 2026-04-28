4b98f4698e830a6fa3d7747001f7bcc00f7ffb40
Lauri Jaatinen <lauri.jaatinen@protonmail.com>
2026-04-28 14:06:27 +0300

    added aws roles

--- modules/storage/s3/main.tf
+++ modules/storage/s3/main.tf
@@ -9,3 +9,44 @@ resource "aws_s3_bucket" "data_platform_bucket" {
   }
 }
 
+#create iam role to access bucket
+resource "aws_iam_role" "deploy_role" {
+  name               = "${var.ENVIRONMENT}-deploy-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "${var.AWS_IAM_USER_DEPLOYER}"
+        }
+      }
+    ]
+  })
+}
+
+#create policy for iam role to access bucket
+resource "aws_iam_policy" "bucket_policy" {
+  name        = "${var.ENVIRONMENT}-bucket-policy"
+  description = "Allow access to ${var.AWS_BUCKET}"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:*"]
+        Effect   = "Allow"
+        Resource = [
+          aws_s3_bucket.data_platform_bucket.arn,
+          "${aws_s3_bucket.data_platform_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+#attach the policy to iam role
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.deploy_role.name
+  policy_arn = aws_iam_policy.bucket_policy.arn
+}
